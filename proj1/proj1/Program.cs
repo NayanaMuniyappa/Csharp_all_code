050f9e21c52e2dc0bf8bdef1a5b0316919dd5951
Nayana Muniyappa <nayanam@infinite.com>
2023-09-11 11:40:17 +0530

    all assignments

---
+++ proj1/proj1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proj1
+{
+    class Program
+    {
+        static int a, b;
+        public static void equal()
+        {
+            WriteLine(" enter the number1");
+            a = Convert.ToInt32(Console.ReadLine());
+            WriteLine("enter the number2");
+            b = Convert.ToInt32(Console.ReadLine());
+            if (a == b)
+            {
+                WriteLine(" equal");
+            }
+            else
+            {
+                WriteLine(" not equal");
+            }
+
+        }
+        public static void negative(params int[] args)
+        {
+            foreach (int i in args)
+            {
+                if (i < 0)
+                {
+                    WriteLine($" {i} is negative ");
+                }
+                else
+                {
+                    WriteLine($" {i} is positive ");
+                }
+            }
+        }
+        public void iteration()
+
+        {
+            WriteLine("start ");
+            int ? i = 0;
+            int ? str = 0;
+            for (i = 10;str!=0;i++)
+            {
+                str = Convert.ToInt32(ReadLine());
+                str += str;
+            }
+        }
+        public static void operation()
+        {
+            WriteLine(" enter the values for operation");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            int add = a + b;
+            int sub = a - b;
+            int multiplication = a * b;
+            float division = a / b;
+            WriteLine($"sum {add} difference {sub} product {multiplication} division {division}");
+        }
+        static void Main(string[] args)
+        {
+            ////equal();
+            ////operation();
+            //negative(1, 2);
+            Program p1 = new Program();
+            p1.iteration();
+
+            Read();
+
+
+        }
+    }
+}
---
+++ proj1/train/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Employee
+    {
+       static int ID,i;
+        String Name;
+        int SALARY;
+        Employee s1 = new Employee();
+        Employee s2 = new Employee();
+        public static void ref_method(ref  int i)
+        {
+            i= 100;
+            WriteLine($" {i} ");
+        }
+        public static int Calculate(int a,int b,out int sum,out int diff,out int prod,out float divi)
+        {
+            sum = a + b;
+            diff = a - b;
+            prod = a * b;
+            divi = a / b;
+            return 0;
+            
+        }
+        public static void Value(int j)
+        {
+           // j = 100;
+            WriteLine(" {0} ", j);
+        }
+       public static int para_meter(params int[] args)
+        {
+            int sum = 0;
+            foreach (int i in args)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+        public static void Main(string[] args)
+        {
+            Value(10);
+            i = 10;
+           ref_method( ref i);
+            WriteLine($" {i} ");
+            int total = 0, d = 0, p = 0;
+            float di = 0;
+           Calculate(10, 5, out total, out d, out p, out di);
+            WriteLine("{0} {1} {2} {3}", total, d, p, di);
+            WriteLine("enter the input to total");
+            ReadLine();
+            int total1 = para_meter(1,2,3,4,5,6,7,8);
+            WriteLine(" {0} ",total1);
+            Read();
+        }
+       
+    }
+}
