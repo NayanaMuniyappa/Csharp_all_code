e5e40b8bf8c9f6f8ac53b70c154bdcc8c9f93745
Nayana Muniyappa <nayanam@infinite.com>
2023-10-03 18:06:13 +0530

    test4

---
+++ Test/Codebasetest_4/Codebasetest_4/Delegates_test.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+delegate int CalculatorOperation(int num1, int num2);
+
+class Calculator
+{
+    public int Add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+    public int Subtract(int num1, int num2)
+    {
+        return num1 - num2;
+    }
+    public int Multiply(int num1, int num2)
+    {
+        return num1 * num2;
+    }
+}
+
+class Delegates_test
+{
+    static void Main()
+    {
+        Calculator calculator = new Calculator();
+
+        Console.WriteLine("Select an operation:");
+        Console.WriteLine("1. Addition");
+        Console.WriteLine("2. Subtraction");
+        Console.WriteLine("3. Multiplication");
+        Console.WriteLine("Enter choice");
+        char c = Convert.ToChar(Console.ReadLine());
+        Console.Write("Enter number1:");
+        int n1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter number2:");
+        int n2 = Convert.ToInt32(Console.ReadLine());
+        CalculatorOperation cc = null;
+            switch (c)
+            {
+                case '1':
+                    cc = calculator.Add;
+                    Console.WriteLine(cc(n1, n2));
+                    break;
+                case '2':
+                    cc = calculator.Subtract;
+                    Console.WriteLine(cc(n1, n2));
+                    break;
+                case '3':
+                    cc = calculator.Multiply;
+                    Console.WriteLine(cc(n1, n2));
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please select a valid operation.");
+                    return;
+            }
+        Console.Read();
+    }   
+}
---
+++ Test/Codebasetest_4/Codebasetest_4/File_test.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codebasetest_4
+{
+    class File_test
+    {
+       public static void Main(string[] args)
+       {
+            writedata();
+            readdata();
+            Read();
+
+       }
+        public static void readdata()
+        {
+            FileStream fs = new FileStream(@"D:\CSharp_Training_all_day\Testdoc.txt ", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            sr.BaseStream.Seek(0, SeekOrigin.Current);
+            string data = sr.ReadLine();
+            while (data != null)
+            {
+                WriteLine(data);
+                data = sr.ReadLine();
+
+            }
+            sr.Close();
+            fs.Close();
+
+
+        }
+
+
+        public static void writedata()
+            {
+                FileStream fs = new FileStream(@"D:\CSharp_Training_all_day\Testdoc.txt", FileMode.Open, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fs);
+                WriteLine("enter the data to file");
+                string data = ReadLine();
+                sw.Write(data);
+                Console.WriteLine("text added successfully");
+                sw.Close();
+                fs.Close();
+
+
+            }
+        }
+    }
