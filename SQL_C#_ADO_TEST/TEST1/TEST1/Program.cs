3c32805ad5bffc1f85193db8fe018084b64c24b7
Nayana Muniyappa <nayanam@infinite.com>
2023-10-23 13:10:34 +0530

    sql

--- SQL_C#_ADO_TEST/TEST1/TEST1/Program.cs
+++ SQL_C#_ADO_TEST/TEST1/TEST1/Program.cs
@@ -10,32 +10,58 @@ namespace TEST1
 {
     class Program
     {
-        public static SqlConnection conn = null;
-        public  static SqlCommand cmd = null;
+        public static SqlConnection conn;
+        public static SqlCommand cmd;
+        public static SqlDataReader dr;
+       
+        public static SqlConnection getconnection()
+        {
+            conn = new SqlConnection("Data Source=ICS-LT-HPNK9K3;Initial catalog=SQL_ADO_TEST;Integrated Security=True");
+          
+            conn.Open();
+            return conn;
+
+        }
+        public static void display()
+        {
+            conn = getconnection();
+            cmd = new SqlCommand("SELECT * FROM Code_Employee", conn);
+            SqlDataReader dr = cmd.ExecuteReader();
+            Console.WriteLine("Employee Records:");
+            while (dr.Read())
+            {
+
+                Console.WriteLine($"Employee ID: {dr["empno"]}, Name: {dr["empname"]}, Salary: {dr["empsal"]}, Type: {dr["emptype"]}");
+
+            }
+            conn.Close();
+
+        }
         public static void Main()
         {
             try
             {
-                SqlConnection conn = new SqlConnection("Data Source=ICS-LT-HPNK9K3;Initial catalog=SQL_ADO_TEST"
-                        + "Integrated Security=True");
-                conn.Open();
-               
-                cmd = new SqlCommand("AddEmployee @empname,@empsal,@emptype", conn);
+
+                conn = getconnection();
+
+                cmd = new SqlCommand(" Addemplyoee", conn);
                 cmd.CommandType = CommandType.StoredProcedure;
                 cmd.Parameters.AddWithValue("@empname", "veena");
                 cmd.Parameters.AddWithValue("@empsal", 30000.00);
                 cmd.Parameters.AddWithValue("@emptype", "F");
-                    cmd.ExecuteNonQuery();
+                cmd.ExecuteNonQuery();
+                
             }
             catch (SqlException e)
             {
                 Console.WriteLine(e.Message);
             }
-           
+            display();
+
             Console.Read();
         }
 
-     }
+    }
 
 }
 
